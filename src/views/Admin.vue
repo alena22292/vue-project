5358a80116238bc866a9da65a6e1f8508a910a74
Alena Gritsiuk <alena22292@mail.ru>
2021-03-28 19:12:02 +0200

    Add a router guard to declare the path and access only for admins

---
+++ src/views/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="admin">
+        It is an Admin!
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Admin'
+}
+</script>
+
+<style lang="scss" scoped>
+  .admin {
+      margin: 50px 5%;
+  }
+</style>
